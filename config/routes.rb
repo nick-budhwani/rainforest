05b4d8c2b59ce279c2b90a9f6dd10069611add71
nadir budhwani <deniro0@yahoo.com>
2016-02-01 16:57:19 -0500

    added nested resource reviews under products

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
-
   root 'products#index'
+
   resources :products
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
+    
 end
