702ab38a9b686dcbfab9c9772f99a95ed6e6e83d
nadir budhwani <deniro0@yahoo.com>
2016-02-01 14:31:59 -0500

    made actions for product controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create!(
+:name => "hairspray",
+:description => "super strong",
+:price_in_cents => 12.50
+)
+
+Product.create!(
+:name => "deoderant",
+:description => "antiperspirant",
+:price_in_cents => 5.00
+)
+
+Product.create!(
+:name => "toothbrush",
+:description => "green medium",
+:price_in_cents => 2.50
+)
